b15d56af65a1078424c3925895d88e2a57f4cd42
yuttapong <zamoko@hotmail.com>
2024-11-09 18:23:43 +0700

    test orm

--- src/bills/bills.ts
+++ src/bills/bills.ts
@@ -8,7 +8,11 @@ import {
   text,
   date,
   decimal,
+  mysqlSchema,
 } from 'drizzle-orm/mysql-core';
+
+export const Bill = mysqlSchema("bill")
+
 export const bills = mysqlTable('bill', {
   id: serial('id').primaryKey(),
   code: varchar('code', { length: 60 }),
@@ -19,11 +23,14 @@ export const bills = mysqlTable('bill', {
   sender_name: varchar('sender_name', { length: 120 }),
   bill_status: varchar('bill_status', { length: 30 }),
 });
+
 export const billItems = mysqlTable('bill_items', {
   item_id: serial('item_id').primaryKey(),
   item_code: varchar('item_code', { length: 60 }),
   bill_id: int('bill_id'),
   description: text('description'),
 });
+
 export type IBill = typeof bills.$inferSelect; // return type when queried
+
 export type INewBill = typeof bills.$inferInsert; // insert type
--- src/bills/index.ts
+++ src/bills/index.ts
@@ -1,11 +1,11 @@
-import { Elysia } from 'elysia';
-import { IBill, bills } from './bills';
-import { db } from '../db';
-import { eq, gt, lt, desc, asc } from 'drizzle-orm';
+import { Elysia } from "elysia";
+import { IBill, bills } from "./bills";
+import { db } from "../db";
+import { eq, gt, lt, desc, asc } from "drizzle-orm";
 
-export const billRoutes = new Elysia().group('/bills', (app) =>
+export const billRoutes = new Elysia().group("/bills", (app) =>
   app
-    .get('/', async ({ set, query }) => {
+    .get("/", async ({ set, query }) => {
       set.status = 200;
       let page = Number(query?.page) || 1;
       let limit = Number(query?.limit) || 20;
@@ -21,11 +21,31 @@ export const billRoutes = new Elysia().group('/bills', (app) =>
         items: result || [],
       };
     })
-    .get('/:id', async ({ path, params, query, response }) => {
+    .get("/manage", async ({ query }) => {
+      let page = Number(query?.page) || 1;
+      let limit = Number(query?.limit) || 15;
+      let order = (query.order)
+      const result: IBill[] = await nextBillPage('desc', limit);
+      return {
+        page: page,
+        limit: limit,
+        items: result || [],
+      };
+    })
+    .get("/:id", async ({ path, params, query, response }) => {
       const result: IBill[] = await db
         .select()
         .from(bills)
-        .where(eq(bills.id, params.id));
+        .where(eq(bills.id, Number(params.id)));
       return result;
     })
 );
+
+const nextBillPage = async (order: 'asc' | 'desc' = 'asc', cursor?: number, pageSize = 3) => {
+  return await db
+    .select()
+    .from(bills)
+    .where(cursor ? (order === 'asc' ? gt(bills.id, cursor) : lt(bills.id, cursor)) : undefined)
+    .limit(pageSize) // the number of rows to return
+    .orderBy(order === 'asc' ? asc(bills.id) : desc(bills.id));
+};
